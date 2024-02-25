b8a61dabebc5a4a0a46916cd12b17d530c6f1098
NalumachuSrikanth1 <62587905+SRIKANTHNALUMACHU@users.noreply.github.com>
2024-02-26 01:31:18 +0530

    remove unnecessary code

--- src/pages/Calendar/index.tsx
+++ src/pages/Calendar/index.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
-//import events from "./events";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import { Box } from "@chakra-ui/react";
 import { useToast } from "@chakra-ui/react";
